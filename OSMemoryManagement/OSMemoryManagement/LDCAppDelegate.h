6ee80fcd6d76e34ead902181e853fafbec49cd99
Ocean <dachang221@126.com>
2013-11-18 19:59:51 +0800

    INIT Commit

---
+++ OSMemoryManagement/OSMemoryManagement/LDCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LDCAppDelegate.h
+//  OSMemoryManagement
+//
+//  Created by 大畅 on 13-11-18.
+//  Copyright (c) 2013年 大畅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LDCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OSMemoryManagement/OSMemoryManagement/LDCViewController.h
@@ -0,0 +1,13 @@
+//
+//  LDCViewController.h
+//  OSMemoryManagement
+//
+//  Created by 大畅 on 13-11-18.
+//  Copyright (c) 2013年 大畅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LDCViewController : UIViewController
+
+@end
